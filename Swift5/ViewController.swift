5c631f502b224cd530ff0f27073893396ea7821a
Hollis Cheng <hollischeng@nwd.com.hk>
2019-04-11 17:59:03 +0800

    Updated

--- Swift5/ViewController.swift
+++ Swift5/ViewController.swift
@@ -10,16 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lblCurrentLanguage: UILabel!
+    @IBOutlet weak var swTCHIAndENG: UISwitch!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+         lblCurrentLanguage.text = Locale.current.languageCode
+        
+        if(Locale.current.languageCode=="en"){
+            swTCHIAndENG.isOn = true
+        }else{
+            swTCHIAndENG.isOn = false
+        }
     }
 
     @IBAction func showMessage(){
-        let alertController = UIAlertController(title: "Welcome to Swift5", message: "HI Hollis CHENG", preferredStyle: UIAlertController.Style.alert)
-        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        let alertController = UIAlertController(title: NSLocalizedString("HelloMsg", comment:""), message: "HI Hollis CHENG", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .default, handler: { _ in
+            NSLog("The \"OK\" alert occured.")
+        }))
         self.present(alertController, animated: true, completion: nil)
     }
 
+    @IBAction func switchLanguage(_ sender:UISwitch){
+        if(sender.isOn == true){
+            print("UISwitch is now ON")
+        }else{
+            print("UISwitch is now OFF")
+        }
+    }
+    
 }
 
