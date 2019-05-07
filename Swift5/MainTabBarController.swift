06a53404abba6c7889692073b4e477feacf745ad
Hollis Cheng <hollischeng@nwd.com.hk>
2019-05-07 17:17:45 +0800

    Updated custom tab

--- Swift5/MainTabBarController.swift
+++ Swift5/MainTabBarController.swift
@@ -11,6 +11,7 @@ import UIKit
 class MainTabBarController: UITabBarController {
 
     var tabBarItems = UITabBarItem()
+    let customButton = UIButton()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,11 +25,13 @@ class MainTabBarController: UITabBarController {
         tabBarItems.image = deSelectedImage1
         tabBarItems.selectedImage = selectedImage1
         
-        let selectedImage2 = UIImage(named:"assetPriviliegesIconInactive")?.withRenderingMode(.alwaysOriginal)
-        let deSelectedImage2 = UIImage(named:"assetPriviliegesIconInactive")?.withRenderingMode(.alwaysOriginal)
-        tabBarItems = self.tabBar.items![1]
-        tabBarItems.image = deSelectedImage2
-        tabBarItems.selectedImage = selectedImage2
+//        let selectedImage2 = UIImage(named:"assetPriviliegesIconInactive")?.withRenderingMode(.alwaysOriginal)
+//        let deSelectedImage2 = UIImage(named:"assetPriviliegesIconInactive")?.withRenderingMode(.alwaysOriginal)
+//        tabBarItems = self.tabBar.items![1]
+//        tabBarItems.image = deSelectedImage2
+//        tabBarItems.selectedImage = selectedImage2
+        
+        settingButton()
         
         let selectedImage3 = UIImage(named:"assetResidentIconInactive")?.withRenderingMode(.alwaysOriginal)
         let deSelectedImage3 = UIImage(named:"assetResidentIconInactive")?.withRenderingMode(.alwaysOriginal)
@@ -45,6 +48,14 @@ class MainTabBarController: UITabBarController {
         // Do any additional setup after loading the view.
     }
     
+    override func tabBar(_ tabBar: UITabBar, didSelect item: UITabBarItem) {
+        // 判斷點選的頁面 title 是否為 Home：true 按鈕背景色為白色；false 則為灰色。
+        if item.title == "Home" {
+            customButton.backgroundColor = .red
+        } else {
+            customButton.backgroundColor = .gray
+        }
+    }
 
     /*
     // MARK: - Navigation
@@ -55,7 +66,50 @@ class MainTabBarController: UITabBarController {
         // Pass the selected object to the new view controller.
     }
     */
+ 
+    func settingButton() {
+        let image = UIImage(named: "assetPriviliegesIconInactive")
+        customButton.setImage(image, for: .normal)
+        customButton.frame.size = CGSize(width: 60, height: 60)
+        // 這邊希望他超出 tabBar 範圍，因此在這邊提高其 y 軸位置。
+        customButton.center = CGPoint(x: tabBar.bounds.midX, y: tabBar.bounds.midY - customButton.frame.height / 3)
+        customButton.backgroundColor = .gray
+        customButton.layer.cornerRadius = 15
+        customButton.layer.borderColor = UIColor.black.cgColor
+        customButton.layer.borderWidth = 3
+        customButton.clipsToBounds = true
+        // 取消按鈕點選 highLight 效果
+        customButton.adjustsImageWhenHighlighted = false
+        // 為客製化按鈕新增一個點擊事件
+        customButton.addTarget(self, action: #selector(showViewController), for: .touchDown)
+        tabBar.addSubview(customButton)
+    }
     
+    @objc func showViewController() {
+        // 設置按鈕背景色，讓他看起來有 highlighted 的效果
+        customButton.backgroundColor = .red
+        // 跳轉至 tabBarController 相對應的索引值的
+        self.selectedIndex = 1
+    }
+    
+   
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        touches.forEach { (touch) in
+            // position 為碰觸的位置
+            let position = touch.location(in: tabBar)
+            // 按鈕的高度偏差值
+            let offset = customButton.frame.height / 3
+            // 判斷按鈕 x 軸範圍是否在其中
+            if customButton.frame.minX <= position.x && position.x <= customButton.frame.maxX {
+                // 判斷按鈕 y 軸範圍是否在其中
+                if customButton.frame.minY - offset <= position.y && position.y <= customButton.frame.maxY - offset{
+                    // 執行相同的按鈕方法
+                    showViewController()
+                }
+            }
+        }
+    }
 }
 
 extension UIImage{
